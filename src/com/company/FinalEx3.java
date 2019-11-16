ac49e390beb08cc3ab760ad9a1b04536f7a17e3d
barsi <marina-yri@mail.ru>
2019-11-16 20:54:14 +0300

    итоговое задание № 3

---
+++ src/com/company/FinalEx3.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.Scanner;
+import java.math.*;
+
+public class FinalEx3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите курс доллара");
+        double kursDollar = in.nextDouble();
+        System.out.println("Введите количество рублей");
+        double rubbles = in.nextDouble();
+        double result = rubbles / kursDollar;
+        BigDecimal res = new BigDecimal(result);
+        BigDecimal result1;
+// Using setScale() method
+        result1 = res.setScale(2, RoundingMode.HALF_UP);
+        System.out.println("Итого :" + result1 + "\n");
+
+    }
+}
